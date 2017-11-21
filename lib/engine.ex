acee113f7583e77c8ff9c4ef9fce07c5f2fd85f6
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-11-20 21:46:44 -0500

    base design

--- lib/engine.ex
+++ lib/engine.ex
@@ -1,5 +1,11 @@
 defmodule Engine do
     use GenServer
+    #function to initialize in-memory tables
+    def initTables do
+        #initialize users table with user_id and username
+        userTable = :ets.new(:users, [:set, :private])
+    end
+
     def register(userName) do
         
     end
--- lib/project4.ex
+++ lib/project4.ex
@@ -1,18 +1,37 @@
+defmodule Server do
+  use GenServer 
+  def handle_call(:start, from, state) do
+    Engine.initTables
+  end
+  def handle_call(:register, userName, state) do
+    Engine.register(userName)
+  end
+end
+
+defmodule Client do
+  def start do
+    
+  end
+end
+
 defmodule Project4 do
-  @moduledoc """
-  Documentation for Project4.
-  """
+  def main(args) do
+    role = args
+            |> parse_args
+            |> Enum.at(0)
+    cond do
+      role == "server" ->
+        {:ok, _} = GenServer.start(Server, state, name: :server)
+        GenServer.call(:server, :start, :infinity)
+      role == "client" ->
+        Client.start
+    end
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Project4.hello
-      :world
-
-  """
-  def hello do
-    :world
+  #parsing the input argument
+  defp parse_args(args) do
+    {_, word, _} = args 
+    |> OptionParser.parse(strict: [:integer, :string, :string])
+    word
   end
 end
