7c41a596fccdef0e04d0297caaaaa98e45fc71bc
Aditya Hegde <adityavhegde@gmail.com>
2017-11-26 15:24:12 -0500

    minor refactoring

--- lib/server.ex
+++ lib/server.ex
@@ -3,21 +3,27 @@ defmodule Server do
   Module to handle all the calls to the Server
   """
   use GenServer
+
+  def init(state) do
+    GenServer.start(ReadTweets, :running, name: :readActor1)
+    GenServer.start(ReadTweets, :running, name: :readActor2)
+    GenServer.start(WriteTweet, :running, name: :writeActor1)
+    GenServer.start(WriteTweet, :running, name: :writeActor2)
+    {:ok, state}
+  end
+
   def handle_call(:start, from, state) do
     ServerApi.startNode()
-    #Engine.startServer
-    Engine.initTables
+    Engine.initTables()
     {:reply, :started, state}
   end
-  #handle call for registering a new process,
-  #needs to be handle call only since can't tweet until registered
+  # handle call for registering a new process,
+  # needs to be handle call only since can't tweet until registered
   def handle_call({:register, userName}, clientPid, state) do
-    #IO.puts "registering client"
-    #IO.inspect clientPid |> elem(0)
     Engine.register(clientPid |> elem(0), userName)
     {:reply, :registered, state}
   end
-  #handle_cast to subscribe client to a user
+  # handle_cast to subscribe user/client to another user/client
   def handle_cast({:subscribe, usersToSub, clientPid}, state) do
     # usersToSub is a list of pid's
     usersToSub |> Enum.each(fn(userPid)->
@@ -25,25 +31,16 @@ defmodule Server do
     end)
     {:noreply, state}
   end
-
-  #
-  def handle_cast({:tweet_subscribers, tweetText, userName}, state) do
-    #IO.inspect :ets.lookup(:users, clientId)
-    ServerApi.tweetSubscribers(userName, tweetText)
-    ServerApi.tweetMentions(tweetText)
-    {:noreply, state}
-  end
-
-  def handle_call(:getUsers, from, state) do
-    userNames = Server.keys(:users)
-    {:reply, userNames, state}
-  end
+  #-----------------------------------------------------------------------------
+  # Write and send tweets to subscribers
   def handle_cast({:tweet_subscribers, tweetText, clientId}, state) do
     state = ServerApi.write(state, clientId, tweetText)
     ServerApi.tweetSubscribers(clientId, tweetText)
     ServerApi.tweetMentions(tweetText)
     {:noreply, state}
   end
+  #-----------------------------------------------------------------------------
+  # Handle search requests by clients
   def handle_cast({:search, clientId}, state) do
     state = ServerApi.read(state, {:search, clientId})
     {:noreply, state}
@@ -55,12 +52,5 @@ defmodule Server do
   def handle_cast({:search_mentions, clientId}, state) do
     state = ServerApi.read(state, {:search_mentions, clientId})
     {:noreply, state}
-  end
-  def init(state) do
-    GenServer.start(ReadTweets, :running, name: :readActor1)
-    GenServer.start(ReadTweets, :running, name: :readActor2)
-    GenServer.start(WriteTweet, :running, name: :writeActor1)
-    GenServer.start(WriteTweet, :running, name: :writeActor2)
-    {:ok, state}
   end
 end
